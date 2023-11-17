18fdcc2bc774e2782231cd08abe68362c40c5d33
Sonnie <mgathoni.gathoni9@gmail.com>
2023-11-17 13:00:39 +0300

    more updates

---
+++ Assets/Scripts/Control/IRayCastable.cs
@@ -0,0 +1,8 @@
+/*namespace RPG.Control
+{
+    public interface IRayCastable
+    {
+        CursorType GetCursorType();
+        bool HandleRaycast(PlayerController callingController);
+    }
+}*/
