866f3228314691c3d903656dad0d8375244717c2
Adesegun Koiki <kamar3deen@gmail.com>
2023-11-17 13:45:52 +0100

    player movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -10,17 +10,37 @@ public class Mover : MonoBehaviour
     private GameObject destination;
     private NavMeshAgent agent;
 
+
     // Start is called
-    void Start()
-    {
-        destination = GameObject.FindGameObjectWithTag("Destination");
-        agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(destination.transform.position);
-    }
+    // void Start()
+    // {
+    //     destination = GameObject.FindGameObjectWithTag("Destination");
+    //     agent = GetComponent<NavMeshAgent>();
+    //     agent.SetDestination(destination.transform.position);
+    // }
 
     // Update is called once per frame
-    // void Update()
-    // {
-    //     GetComponent<NavMeshAgent>().destination = target.position;
-    // }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+        // Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+    
+        // agent.SetDestination(destination.transform.position);
+
+        // GetComponent<NavMeshAgent>().destination = target.position;
+    }
+
+    private void MoveToCursor()
+    {
+    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    RaycastHit hit;
+    bool hasHit = Physics.Raycast(ray, out hit);
+    if(hasHit)
+    {
+        GetComponent<NavMeshAgent>().destination = hit.point;
+    }
+    }
 }
